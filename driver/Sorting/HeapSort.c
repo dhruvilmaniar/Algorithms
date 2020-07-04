6bf4545735e412b5931a9468862413e80dc3246c
Dhruvil <dhruvilmaniar@gmail.com>
2020-07-04 19:48:20 +0530

    Refactored HeapSort

--- driver/Sorting/HeapSort.c
+++ driver/Sorting/HeapSort.c
@@ -1,12 +1,13 @@
-#include "../../include/Sorting/HeapSort.h"
+// #include "../../include/Sorting/MaxHeapSort.h"
+#include "../../include/Sorting/MinHeapSort.h"
 
 
 int main()
 {
     int arr[] = {4,2,5,1,77};
 
-    buildMaxHeap(arr, 5);
-    printHeap(arr, 5);
+    // buildMaxHeap(arr, 5);
+    // printHeap(arr, 5);
 
     buildMinHeap(arr, 5);
     printHeap(arr, 5);
---
+++ include/Sorting/MinHeapSort.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+static void swap(int *first, int *second)
+{
+    int temp = *first;
+    *first = *second;
+    *second = temp;
+}
+
+
+void minHeapify(int *arr, int index, int heapSize)
+{
+    int leftChildIndex = 2*index;
+    int rightChildIndex = 2*index + 1;
+
+    int smallest = index;
+
+    if (leftChildIndex < heapSize && arr[leftChildIndex]<arr[index])
+        smallest = leftChildIndex;
+    if (rightChildIndex < heapSize && arr[rightChildIndex]<arr[smallest])
+        smallest = rightChildIndex;
+
+    if (index != smallest)
+    {
+        swap(&arr[smallest], &arr[index]);
+        minHeapify(arr, smallest, heapSize);
+    }
+}
+
+
+void buildMinHeap(int *arr, int arrLength)
+{
+    int i = arrLength/2;
+
+    while(i>=0)
+    {
+        minHeapify(arr, i, arrLength);
+        i--;
+    }
+}
+
+
+int deleteMin(int *arr, int heapSize)
+{
+    if (heapSize < 1)
+    {
+        printf("Heap underflow...\n");
+        return -1;
+    }
+    
+    int min = arr[0];
+
+    swap(&arr[0], &arr[heapSize-1]);
+    heapSize--;
+
+    minHeapify(arr, 0, heapSize);
+    return min;
+}
+
+
+static void printHeap(int *arr, int arrLength)
+{
+    int i = 0;
+    while(i<arrLength)
+        printf("%d ", arr[i++]);
+    printf("\n");
+}
